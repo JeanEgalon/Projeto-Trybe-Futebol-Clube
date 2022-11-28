ae0ab605182d37faff3f8be336d8a39db49ec025
Jean Egalon <egalon.jean@gmail.com>
2022-11-28 17:42:43 -0300

    feito o 20 e 21

--- app/backend/src/database/controllers/MatchController.ts
+++ app/backend/src/database/controllers/MatchController.ts
@@ -3,7 +3,13 @@ import MatchService from '../services/MatchService';
 
 export default class MatchController {
   static async getMatchs(req: Request, res: Response) {
+    if (req.query.inProgress === 'true' || req.query.inProgress === 'false') {
+      const { inProgress } = req.query;
+
+      const result = await MatchService.getMatchsInProgress(inProgress);
+      return res.status(200).json(result);
+    }
     const result = await MatchService.getMatchs();
-    res.status(200).json(result);
+    return res.status(200).json(result);
   }
 }
--- app/backend/src/database/services/MatchService.ts
+++ app/backend/src/database/services/MatchService.ts
@@ -1,14 +1,27 @@
-// import IMatch from '../entities/IMatch';
+import IMatch from '../entities/IMatch';
 import MatchModel from '../models/MatchModel';
 import TeamModel from '../models/TeamModel';
 
 export default class MatchService {
-  static async getMatchs(): Promise<unknown> {
+  static async getMatchs(): Promise<IMatch[] | unknown> {
     const uEmail = await MatchService.findAllMatchs();
+
     return uEmail;
   }
 
-  private static async findAllMatchs(): Promise<unknown> {
+  static async getMatchsInProgress(inProgress: string | unknown): Promise<IMatch[]> {
+    const uEmail = await MatchService.findAllMatchs() as IMatch[];
+    let param = false;
+    if (inProgress === 'false') {
+      param = false;
+    } else { param = true; }
+
+    const getInProgress = uEmail.filter((item) => item.inProgress === param);
+
+    return getInProgress;
+  }
+
+  private static async findAllMatchs(): Promise<IMatch[] | unknown> {
     const userEmail = await MatchModel.findAll({
       include: [
         { model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
